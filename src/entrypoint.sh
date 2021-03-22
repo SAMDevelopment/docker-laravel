c2a0c2cb9ebeaa1232d8fde379419b5edbd7060d
NickVries <nickdevries93@gmail.com>
2021-03-22 18:05:59 +0100

    :whale: :bookmark: Upgraded PHP to PHP8.0 and Composer to Composer 2.

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -6,8 +6,8 @@ Setting up configuration files
 
 substitutions="\${APP_DOMAIN} \${APP_LOG_LEVEL} \${APP_PUBLIC_DIR}"
 
-if [ ! -f /etc/php/7.1/fpm/php-fpm.conf ]; then
-    envsubst "${substitutions}" < /etc/php/7.1/fpm/php-fpm.conf.template > /etc/php/7.1/fpm/php-fpm.conf
+if [ ! -f /etc/php/8.0/fpm/php-fpm.conf ]; then
+    envsubst "${substitutions}" < /etc/php/8.0/fpm/php-fpm.conf.template > /etc/php/8.0/fpm/php-fpm.conf
 fi
 
 if [ ! -f /etc/nginx/sites-available/default ]; then
@@ -24,15 +24,15 @@ Configuring Xdebug
 
 if [ ! "${DOCKER_ENV}" == "production" ] && [ ! "${DOCKER_ENV}" == "prod" ]; then
     # Enable xdebug
-    ln -sf /etc/php/7.1/mods-available/xdebug.ini /etc/php/7.1/fpm/conf.d/20-xdebug.ini
+    ln -sf /etc/php/8.0/mods-available/xdebug.ini /etc/php/8.0/fpm/conf.d/20-xdebug.ini
 else
     # Disable xdebug
-    if [ -e /etc/php/7.1/fpm/conf.d/20-xdebug.ini ]; then
-        rm -f /etc/php/7.1/fpm/conf.d/20-xdebug.ini
+    if [ -e /etc/php/8.0/fpm/conf.d/20-xdebug.ini ]; then
+        rm -f /etc/php/8.0/fpm/conf.d/20-xdebug.ini
     fi
 fi
 
-sed -i "s/xdebug\.remote_host\=.*/xdebug\.remote_host\=${APP_DOMAIN}/g" /etc/php/7.1/mods-available/xdebug.ini
+sed -i "s/xdebug\.remote_host\=.*/xdebug\.remote_host\=${APP_DOMAIN}/g" /etc/php/8.0/mods-available/xdebug.ini
 
 : '================================================
 Continue with passed in command
